aeda17543225f92cebc7d7ae2ad8c278befa9efc
odhiambo_athooh <s.athooh@gmail.com>
2023-05-25 23:42:40 +0300

    contact and startup pages updated

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('startup/', views.startup, name='startup'),
+    path('contact/', views.contact, name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def startup(request):
+    return render(request, 'pages/startup.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')

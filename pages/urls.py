9795dc77c844ba0e88999cb25c0d7d73e616c964
odhiambo_athooh <s.athooh@gmail.com>
2023-06-08 15:24:29 +0300

    about and careers pages

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('startup/', views.startup, name='startup'),
+    path('talent/', views.talent, name='talent'),
     path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog, name='blog')
+    path('blog/', views.blog, name='blog'),
+    path('about/', views.about, name='about'),
+    path('careers/', views.careers, name='careers')
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,8 +7,17 @@ def index(request):
 def startup(request):
     return render(request, 'pages/startup.html')
 
+def talent(request):
+    return render(request, 'pages/talent.html')
+
 def contact(request):
     return render(request, 'pages/contact.html')
 
 def blog(request):
     return render(request, 'pages/blog.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def careers(request):
+    return render(request, 'pages/careers.html')
